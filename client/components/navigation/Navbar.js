64fcb044b558a865c1ba22f22cfe2d369fc62611
afkingshuk <afkingshuk@gmail.com>
2019-11-13 19:09:25 +0600

    loader

--- client/components/navigation/Navbar.js
+++ client/components/navigation/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { 
+import {
   Link,
   withRouter
 } from 'react-router-dom';
@@ -34,8 +34,8 @@ class Navbar extends Component {
   }
 
   handleClickOutside(e) {
-    if(this.wrappedRef && this.picture && !this.wrappedRef.contains(e.target) && !this.picture.contains(e.target)) {
-      this.setState({dropdown: false});
+    if (this.wrappedRef && this.picture && !this.wrappedRef.contains(e.target) && !this.picture.contains(e.target)) {
+      this.setState({ dropdown: false });
     }
   }
 
@@ -63,37 +63,37 @@ class Navbar extends Component {
 
   onChangeSearch = (e) => {
     this.setState({ search: e.target.value });
-    if(e.target.value.length == 0) {
-      this.setState({ searchUsers: []});
+    if (e.target.value.length == 0) {
+      this.setState({ searchUsers: [] });
       return;
     }
-    axios.get(`/api/users`, { params: { text: e.target.value, isFriend: true }})
-    .then(
-      response => this.setState({ searchUsers: response.data }),
-      error => console.log('An error occurred: ', error)
-    )
+    axios.get(`/api/users`, { params: { text: e.target.value, isFriend: true } })
+      .then(
+        response => this.setState({ searchUsers: response.data }),
+        error => console.log('An error occurred: ', error)
+      )
   }
 
   addFriend = (id) => {
     axios.put(`/api/users/${this.props.auth.user._id}/friends/${id}`)
-    .then(response => { this.onChangeSearch({ target: { value: this.state.search }}) })
-    .catch(err => console.log('An error occurred: ', err));
+      .then(response => { this.onChangeSearch({ target: { value: this.state.search } }) })
+      .catch(err => console.log('An error occurred: ', err));
   }
 
   deleteFriend = (id) => {
     console.log('deleting: ', id);
     console.log('deleting');
     axios.delete(`/api/users/${this.props.auth.user._id}/friends/${id}`)
-    .then(response => { this.onChangeSearch({ target: { value: this.state.search }}) })
-    .catch(err => console.log('An error occurred: ', err));
+      .then(response => { this.onChangeSearch({ target: { value: this.state.search } }) })
+      .catch(err => console.log('An error occurred: ', err));
   }
 
   render() {
     const { isAuthenticated } = this.props.auth;
     const { username, _id } = this.props.auth.user;
-    const renderNavbar = !(this.props.location.pathname == '/login' || this.props.location.pathname =='/register');
+    const renderNavbar = !(this.props.location.pathname == '/login' || this.props.location.pathname == '/register');
 
-    if(!renderNavbar)
+    if (!renderNavbar)
       return null;
 
     const collapseClass = this.state.collapsed ? 'collapse' : '';
@@ -102,9 +102,9 @@ class Navbar extends Component {
       <ul className="navbar-nav navbar-right">
         <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="#" id="navbar-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onClick={this.toggleDropdown}>
-            <img ref={this.setPicture} class="rounded-circle" src="/img/profile.jpg"/>
+            <img ref={this.setPicture} class="rounded-circle" src="/img/profile.jpg" />
           </a>
-          <div ref={this.setWraperRef} className="dropdown-menu dropdown-menu-right" aria-labelledby="navbar-dropdown" style={ this.state.dropdown ? {display: 'block'} : {display: 'none'}}>
+          <div ref={this.setWraperRef} className="dropdown-menu dropdown-menu-right" aria-labelledby="navbar-dropdown" style={this.state.dropdown ? { display: 'block' } : { display: 'none' }}>
             <span className="dropdown-header">Signed in as</span>
             <h6 className="dropdown-header"><b>{username}</b></h6>
             <div className="dropdown-divider"></div>
@@ -134,35 +134,48 @@ class Navbar extends Component {
           <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation" onClick={this.toggleCollapse}>
             <span className="navbar-toggler-icon"></span>
           </button>
-          <Link className="navbar-brand" to="/">            
-          <img src='/img/logo.png' alt="Logo" style={{width:100}}/>      
-          </Link>
-          <Link className="navbar-brand" to="/about-us">            
-          About Us     
-          </Link>
-            <input className="navbar-search form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={this.onChangeSearch}/>
-            <div className="navbar-search-result" style={{display: searchResult,borderRadius:null}}>
-              <ul>
-                {
-                  this.state.searchUsers.map(user => (
-                    <li key={user._id}>
-                      { _id !== user._id && user.isFriend === false &&
-                        <span className="navbar-search-result-button fa fa-user-plus" onClick={() => this.addFriend(user._id)}></span>
-                      }
-                      { _id !== user._id && user.isFriend === true &&
-                        <span className="navbar-search-result-button fa fa-user-times" onClick={() => this.deleteFriend(user._id)}></span>
-                      }
-                      <img src="/img/profile.jpg"/>
-                      <h6>{ user.fullName ? user.fullName : user.username }</h6>
-                      <span className="text-muted">@{user.username}</span>
-                    </li>
-                  ))
-                }
-              </ul>
-            </div>
+          {
+            isAuthenticated ?
+              <div>
+                <Link className="navbar-brand" to="/projects">
+                  <img src='/img/logo.png' alt="Logo" style={{ width: 175, marginRight: 25 }} />
+                </Link>
+
+                <Link className="navbar-brand" to="/about-us">
+                  About Us
+              </Link>
+              </div>
+              :
+              <div>
+
+                <img src='/img/logo.png' alt="Logo" style={{ width: 100 }} />
+
+              </div>
+          }
+
+          <input className="navbar-search form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={this.onChangeSearch} />
+          <div className="navbar-search-result" style={{ display: searchResult, borderRadius: null }}>
+            <ul>
+              {
+                this.state.searchUsers.map(user => (
+                  <li key={user._id}>
+                    {_id !== user._id && user.isFriend === false &&
+                      <span className="navbar-search-result-button fa fa-user-plus" onClick={() => this.addFriend(user._id)}></span>
+                    }
+                    {_id !== user._id && user.isFriend === true &&
+                      <span className="navbar-search-result-button fa fa-user-times" onClick={() => this.deleteFriend(user._id)}></span>
+                    }
+                    <img src="/img/profile.jpg" />
+                    <h6>{user.fullName ? user.fullName : user.username}</h6>
+                    <span className="text-muted">@{user.username}</span>
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
           <div className={"navbar-collapse " + collapseClass} id="navigation">
             <ul className="navbar-nav mr-auto"></ul>
-            { isAuthenticated ? userLinks : guestLinks }
+            {isAuthenticated ? userLinks : guestLinks}
           </div>
         </div>
       </nav>
