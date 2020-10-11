7882946fd44bf40c5a2aa09a68c74658570538e8
Kingshuk <afkingshuk@gmail.com>
2020-10-12 01:40:37 +0600

    mongodb atlas

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const webpackMiddleware = require('webpack-dev-middleware');
 import webpackConfig from '../webpack.config.dev.js';
 
 // Database
-mongoose.connect( process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tboard', {
+mongoose.connect( process.env.MONGODB_URI || 'mongodb+srv://tboard:<password>@afkingshuk.btuaw.mongodb.net/<dbname>?retryWrites=true&w=majority', {
   useNewUrlParser: true
 });
 
